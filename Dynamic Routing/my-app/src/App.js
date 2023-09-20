e0f2e9fd6b0209ef6f87aa62abedf5db4c54cc29
12Harsha2000 <harshamariajose.20@gmail.com>
2023-09-20 14:05:31 +0530

    http

--- Dynamic Routing/my-app/src/App.js
+++ Dynamic Routing/my-app/src/App.js
@@ -1,25 +1,52 @@
-import React from "react";
+// import React from "react";
 
-function App(){
-  let user=[
-    {name:'Nila',id:1},
-    {name:'Niya',id:2},
-    {name:'Nima',id:3},
-    {name:'Nita',id:4},
-  ]
-  return(
-    <div className="App">
-      <h1>React Dynamic Routing</h1>
-      {
-        user.map((item)=>
-        <div><a href="/user">{item.name}</a></div>)
-      }
-    </div>
-  );
-}
+// function App(){
+//   let user=[
+//     {name:'Nila',id:1},
+//     {name:'Niya',id:2},
+//     {name:'Nima',id:3},
+//     {name:'Nita',id:4},
+//   ]
+//   return(
+//     <div className="App">
+//       <h1>React Dynamic Routing</h1>
+//       {
+//         user.map((item)=>
+//         <div><a href="/user">{item.name}</a></div>)
+//       }
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
+// v need t he value to b displayed in the webpage as in the url..
+
+// import React from "react";
+
+// function App(){
+//   let user=[
+//     {name:'Nila',id:1},
+//     {name:'Niya',id:2},
+//     {name:'Nima',id:3},
+//     {name:'Nita',id:4},
+//   ]
+//   return(
+//     <div className="App">
+//       <h1>React Dynamic Routing</h1>
+//       {
+//         user.map((item)=>
+//         <div><a href={"/user/"+item.id}>{item.name}</a></div>)
+//       }
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// list is actually Refreshing so...
 
 // import React from "react";
 // import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
@@ -48,32 +75,34 @@ export default App;
 
 // export default App;
 
+
+
 // but it is not defined with the routes
 // to define it....
 
-// import React from "react";
-// import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
-// import User from './User'
+import React from "react";
+import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
+import User from './User'
 
-// function App(){
-//   let user=[
-//     {name:'Nila',id:1},
-//     {name:'Niya',id:2},
-//     {name:'Nima',id:3},
-//     {name:'Nita',id:4},
-//   ]
-//   return(
-//     <div className="App">
-//       <Router>
-//       <h1>React Dynamic Routing</h1>
-//       {
-//         user.map((item)=>
-//       <div><Link to={"/user/"+item.id+"/"+item.name}>{item.name}</Link></div>)
-//       }
-//       <Route path="/user/:id/:name"><User /></Route>
-//       </Router>
-//     </div>
-//   )
-// }
+function App(){
+  let user=[
+    {name:'Nila',id:1},
+    {name:'Niya',id:2},
+    {name:'Nima',id:3},
+    {name:'Nita',id:4},
+  ]
+  return(
+    <div className="App">
+      <Router>
+      <h1>React Dynamic Routing</h1>
+      {
+        user.map((item)=>
+      <div><Link to={"/user/"+item.id+"/"+item.name}>{item.name}</Link></div>)
+      }
+      <Route path="/user/:id/:name"><User /></Route>
+      </Router>
+    </div>
+  )
+}
 
-// export default App;
+export default App;
--- Dynamic Routing/my-app/src/User.js
+++ Dynamic Routing/my-app/src/User.js
@@ -1,23 +1,23 @@
-// // import React from 'react-router-dom'
+// // // import React from 'react-router-dom'
 
-// // function User() {
-// //   return (
-// //     <div><h1>User Component</h1></div>
-// //   )
-// // }
-
-// // export default User;
-
-
-
-// import {withRouter} from 'react-router-dom'
-
-// function User(props) 
-// {
-//     console.warn(props)
-//     return (
-//     <div><h1>User No {props.match.params.id}</h1>
-//     <h1>User Name {props.match.params.name}</h1></div>
-//       )
+// function User() {
+//   return (
+//     <div><h1>User Component</h1></div>
+//   )
 // }
-// export default withRouter(User);
+
+// export default User;
+
+
+
+import {withRouter} from "react-router-dom"
+
+function User(props) 
+{
+    console.warn(props)
+    return (
+    <div><h1>User No {props.match.params.id}</h1>
+    <h1>User Name {props.match.params.name}</h1></div>
+    )
+}
+export default withRouter(User);
